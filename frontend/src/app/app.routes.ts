e8072f7adea0308f090a8d289596942a6f3407ec
Yanic Dober <yanic.dobler@gmail.com>
2024-01-27 19:53:26 +0100

    Add home component and added routing Removed .spec files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
+import {NxWelcomeComponent} from "./nx-welcome.component";
+import {HomeComponent} from "./home/home.component";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'test',
+    loadComponent: () => import('@frontend/ui').then((m) => m.UiComponent)
+  }
+];
