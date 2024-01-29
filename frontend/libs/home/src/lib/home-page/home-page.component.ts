026be48184fd90b83302bce6c109944f9d19fe4d
Yanic Dober <yanic.dobler@gmail.com>
2024-01-29 17:39:00 +0100

    Add home lib

---
+++ frontend/libs/home/src/lib/home-page/home-page.component.ts
@@ -0,0 +1,16 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {CardComponent, StandardPageLayoutComponent} from "@frontend/ui";
+import {TranslateModule} from "@ngx-translate/core";
+import {
+  StandardSectionLayoutComponent
+} from "../../../../ui/src/lib/standard-section-layout/standard-section-layout.component";
+
+@Component({
+  selector: 'itd-home',
+  standalone: true,
+  imports: [CommonModule, StandardPageLayoutComponent, CardComponent, TranslateModule, StandardSectionLayoutComponent],
+  templateUrl: './home-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomePageComponent {}
