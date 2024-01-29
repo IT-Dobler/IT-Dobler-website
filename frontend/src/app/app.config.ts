c710243c19c1cf135c66a0d1dffdcbbb00c4421a
Yanic Dober <yanic.dobler@gmail.com>
2024-01-29 17:41:54 +0100

    Intellij config and unused import

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {appRoutes} from './app.routes';
-import {TranslateLoader, TranslateModule, TranslateStore} from "@ngx-translate/core";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
