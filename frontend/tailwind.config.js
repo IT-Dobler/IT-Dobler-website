520f37a4ea03b9f558278b1c6e5ab4720c08c571
Yanic Dober <yanic.dobler@gmail.com>
2024-01-29 17:35:53 +0100

    Translations, Nx configuration and added tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,31 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gold': '#DCA54C',
+        'dark-blue': '#001F29',
+      },
+      backgroundImage: {
+        'bg-illustration': 'url("/assets/bg-illustration.svg")',
+        'bg-illustration-dark': 'url("/assets/bg-illustration-dark.svg")',
+      },
+      fontFamily: {
+        'orbitron': ['Orbitron', 'sans-serif'],
+      },
+      listStyleImage: {
+          'li-point': 'url("/assets/li-point.svg")',
+          'li-point-dark': 'url("/assets/li-point-dark.svg")',
+      },
+    },
+  },
+  plugins: [],
+};
