7a60424ed1247c730c154847bb1db88b16b1d471
Yanic Dober <yanic.dobler@gmail.com>
2024-02-12 00:03:09 +0100

    - Debugging CI/CD

--- api/src/functions/SendEmail.ts
+++ api/src/functions/SendEmail.ts
@@ -1,7 +1,11 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 
+// import {send, setApiKey} from '@sendgrid/mail';
+
 export async function SendEmail(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
+
+    const sgMail = require('@sendgrid/mail');
 
     const name = request.query.get('name') || await request.text() || 'world';
 
