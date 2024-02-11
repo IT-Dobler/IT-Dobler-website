f7c178f2ea81ae6e0054a59f78daeb8e99533725
Yanic Dober <yanic.dobler@gmail.com>
2024-02-11 23:08:09 +0100

    - First attempt at sending an email

--- api/src/functions/SendEmail.ts
+++ api/src/functions/SendEmail.ts
@@ -1,11 +1,24 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-
+import sgMail = require('@sendgrid/mail');
 export async function SendEmail(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
     const name = request.query.get('name') || await request.text() || 'world';
 
-    return { body: `Hello, ${name}!` };
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const response = await sgMail.send({
+        from: 'dobler.it@gmail.com',
+        to: 'dobler.it@gmail.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    })
+
+    console.log(response);
+
+    return { body: `Hello, ${name}! Heres the SendGrid response: ${JSON.stringify(response)}` };
 };
 
 app.http('SendEmail', {
