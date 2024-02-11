12eff92dc963e9383e82287751bcf29f8f0eb867
Yanic Dober <yanic.dobler@gmail.com>
2024-02-12 00:10:10 +0100

    - Debugging CI/CD

--- api/src/functions/SendEmail.ts
+++ api/src/functions/SendEmail.ts
@@ -1,15 +1,25 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-import {send, setApiKey} from '@sendgrid/mail';
+import * as sgMail from '@sendgrid/mail';
 
 export async function SendEmail(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    // const sgMail = require('@sendgrid/mail');
-
     const name = request.query.get('name') || await request.text() || 'world';
 
-    return { body: `Hello, ${name}!` };
-};
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const response = await sgMail.send({
+        from: 'dobler.it@gmail.com',
+        to: 'dobler.it@gmail.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    })
+
+    console.log(response);
+
+    return { body: `Hello, ${name}! Heres the SendGrid response: ${JSON.stringify(response)}` };
+}
 
 app.http('SendEmail', {
     methods: ['GET', 'POST'],
